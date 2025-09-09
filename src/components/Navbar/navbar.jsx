ddea2fbb3b3dbb5f36ea25497c02bafeda27a340
unknown <humb220106>
2025-09-09 17:22:55 +0100

    changes are made

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">CampusConnect</h2>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/feedback">Feedback</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
