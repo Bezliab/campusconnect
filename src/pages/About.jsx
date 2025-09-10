b982ad5c7fdd7b90920b7c5ef1de080783ee6696
Ahmaakor <ahmadakorede04@gmail.com>
2025-09-10 04:16:23 -0700

    about without media query

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,9 @@
 import React from "react";
+import AboutMain from "../components/AboutMain/AboutMain";
 
 const About = () => {
   return (
-    <div>
-      <h1>About CampusConnect</h1>
-      <p>
-        CampusConnect is your hub for campus events, galleries, feedback, and more.
-        Stay connected and discover what's happening around you!
-      </p>
-    </div>
+    <AboutMain />
   );
 };
 
