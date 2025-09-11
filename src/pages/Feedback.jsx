38f66e337f1e0d2c084f1c175a40dac3416df1b3
Musty-16 <abassmustapha31@gmail.com>
2025-09-11 15:16:16 +0100

    changes made contact and feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-
-// const Feedback = () => {
-//   return (
-//     <div>
-//       <h1>About CampusConnect</h1>
-//       <p>
-//         CampusConnect is your hub for campus events, galleries, feedback, and more.
-//         Stay connected and discover what's happening around you!
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default Feedback;
-
 "use client"
 
 import { useState } from "react"
