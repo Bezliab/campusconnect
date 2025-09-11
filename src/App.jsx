1e6b9e5fadf07f959faa05f541628dd8f73c3e55
Cybernaut12 <ajisolavictor1@gmail.com>
2025-09-11 16:34:04 +0100

    cybernaut work perfect 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Gallery from "./pages/Gallery";
 import Feedback from "./pages/Feedback";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer/footer";
+import EventDetails from "./pages/EventDetails";
+import BackToTop from "./components/BacktoTop";
 
 function App() {
   return (
@@ -14,12 +16,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/events/:id" element={<EventDetails />} />
         <Route path="/about" element={<About />} />
         <Route path="/events" element={<Events />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/feedback" element={<Feedback />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <BackToTop />
       <Footer />
     </>
   );
